1b7518cf989a80ea8dab67a85529d8c6ac743fc5
Estevan Jantsk <estevan.jantsk@gmail.com>
2024-03-10 20:00:13 +0200

    rename modules

--- lib/telephony/core/subscriber.ex
+++ lib/telephony/core/subscriber.ex
@@ -1,4 +1,4 @@
-defmodule Subscriber.Subscriber do
+defmodule Telephony.Core.Subscriber do
   defstruct full_name: nil, id: nil, phone_number: nil, subscriber_type: :prepaid
 
   def new(payload) do
