a188a730d54a4b748440415ae0392d7724818994
Estevan Jantsk <estevan.jantsk@scoro.com>
2024-05-08 16:55:36 +0300

    update core

--- lib/telephony/core/core.ex
+++ lib/telephony/core/core.ex
@@ -18,4 +18,27 @@ defmodule Telephony.Core do
   def create_subscriber(_subscribers, _payload) do
     {:error, "Only 'prepaid' or 'postpaid' are accepted"}
   end
+
+  def search_subscriber(subscribers, phone_number) do
+    Enum.find(subscribers, &(&1.phone_number == phone_number))
+  end
+
+  def make_recharge(subscribers, phone_number, value, date) do
+    case subscribers
+         |> search_subscriber(phone_number) do
+      nil -> subscribers
+      subscriber -> make_recharge_and_update_subscribers(subscribers, subscriber, value, date)
+    end
+  end
+
+  defp make_recharge_and_update_subscribers(subscribers, subscriber, value, date) do
+    case Subscriber.make_recharge(subscriber, value, date) do
+      {:error, _} ->
+        subscribers
+
+      updated_subscriber ->
+        subscribers = List.delete(subscribers, subscriber)
+        subscribers ++ [updated_subscriber]
+    end
+  end
 end
--- test/telephony/core/core_test.exs
+++ test/telephony/core/core_test.exs
@@ -10,11 +10,16 @@ defmodule Telephony.CoreTest do
         full_name: "Estevan",
         phone_number: "123",
         type: %Prepaid{credits: 0, recharges: []}
+      },
+      %Subscriber{
+        full_name: "Estevan 2",
+        phone_number: "1232",
+        type: %Prepaid{credits: 1, recharges: []}
       }
     ]
 
     payload = %{
-      full_name: "Estevan",
+      full_name: "Estevan Payload",
       phone_number: "123",
       type: :prepaid
     }
@@ -25,10 +30,19 @@ defmodule Telephony.CoreTest do
     }
   end
 
-  test "create a new subscriber", %{subscribers: subscribers, payload: payload} do
+  test "create a new subscriber", %{payload: payload} do
+    expect = [
+      %Telephony.Core.Subscriber{
+        full_name: "Estevan Payload",
+        phone_number: "123",
+        type: %Telephony.Core.Prepaid{credits: 0, recharges: []},
+        calls: []
+      }
+    ]
+
     result = Core.create_subscriber([], payload)
 
-    assert result == subscribers
+    assert result == expect
   end
 
   test "display error, when subscriber already exist", %{
@@ -45,5 +59,53 @@ defmodule Telephony.CoreTest do
     payload = Map.put(payload, :type, :shausuha)
     result = Core.create_subscriber([], payload)
     {:error, "Only 'prepaid' or 'postpaid' are accepted"} = result
+  end
+
+  test "search a subscriber", %{
+    subscribers: subscribers
+  } do
+    expect = %Subscriber{
+      full_name: "Estevan",
+      phone_number: "123",
+      type: %Prepaid{credits: 0, recharges: []}
+    }
+
+    result = Core.search_subscriber(subscribers, "123")
+    assert expect == result
+  end
+
+  test "return nil if subscriber does not exist", %{
+    subscribers: subscribers
+  } do
+    result = Core.search_subscriber(subscribers, "198")
+    assert result == nil
+  end
+
+  test "make a recharge", %{
+    subscribers: subscribers
+  } do
+    date = ~D[2024-05-08]
+
+    expected = [
+      %Telephony.Core.Subscriber{
+        full_name: "Estevan 2",
+        phone_number: "1232",
+        type: %Telephony.Core.Prepaid{credits: 1, recharges: []},
+        calls: []
+      },
+      %Telephony.Core.Subscriber{
+        full_name: "Estevan",
+        phone_number: "123",
+        type: %Telephony.Core.Prepaid{
+          credits: 2,
+          recharges: [%Telephony.Core.Recharge{value: 2, date: ~D[2024-05-08]}]
+        },
+        calls: []
+      }
+    ]
+
+    result = Core.make_recharge(subscribers, "123", 2, date)
+
+    assert result == expected
   end
 end
